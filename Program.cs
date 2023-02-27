1a830227177c63beb68661ae04fb4d0e0b8a3a4e
Vladimir <saintprize@gmail.com>
2023-02-27 21:56:14 +0300

    add method 'FilterArrayByLength'

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
 ﻿string[] sourceArray = new string[5] {"Kazan", "Ork", "World", "Soul", "res"};
 string[] filteredArray = new string[sourceArray.Length];
 
+void FilterArrayByLength(string[] sourceArray, string[] filteredArray)
+{
+    int count = 0;
+    for (int i = 0; i < sourceArray.Length; i++)
+    {
+        if (sourceArray[i].Length <= 3)
+        {
+            filteredArray[count] = sourceArray[i];
+            count++;
+        }
+    }
+}
+
 FilterArrayByLength(sourceArray, filteredArray);
 PrintArray(filteredArray);
