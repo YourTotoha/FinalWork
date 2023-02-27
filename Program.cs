1260a069c3614411bfca07c1542d44074bb55c75
Vladimir <saintprize@gmail.com>
2023-02-27 21:55:16 +0300

    start work with code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿string[] sourceArray = new string[5] {"Kazan", "Ork", "World", "Soul", "res"};
+string[] filteredArray = new string[sourceArray.Length];
+
+FilterArrayByLength(sourceArray, filteredArray);
+PrintArray(filteredArray);
